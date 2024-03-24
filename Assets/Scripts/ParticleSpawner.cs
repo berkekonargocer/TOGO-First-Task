ab1d0d13c85eb2cc72b9dffb1e512f9d32140a20
Berke Konargöçer <berke.konargocer@gmail.com>
2024-03-24 11:39:18 +0300

    Particle Object Destroy Fixed

--- Assets/Scripts/ParticleSpawner.cs
+++ Assets/Scripts/ParticleSpawner.cs
@@ -6,6 +6,7 @@ public class ParticleSpawner : MonoBehaviour
     public void SpawnParticle(ParticleSystem particlePrefab) {
         ParticleSystem particle = Instantiate(particlePrefab, transform.position, Quaternion.identity);
         particle.Play();
-        Destroy(particle, particle.main.startLifetime.constant);
+        float destroyWaitTime = particle.main.startLifetime.constant;
+        Destroy(particle.gameObject, destroyWaitTime);
     }
 }

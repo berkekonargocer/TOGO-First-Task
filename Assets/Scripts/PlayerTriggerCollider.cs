9c7f65e8cff729f4a92ab4e2fc58d464569857e5
Berke Konargöçer <berke.konargocer@gmail.com>
2024-03-24 19:44:03 +0300

    Assembly Definitions

--- Assets/Scripts/PlayerTriggerCollider.cs
+++ Assets/Scripts/PlayerTriggerCollider.cs
@@ -3,19 +3,11 @@ using UnityEngine.Pool;
 
 public class PlayerTriggerCollider : MonoBehaviour
 {
-    //[SerializeField] IObjectPool<Road> roadPool;
-
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Collectable")) 
         {
             ICollectable collectable = other.gameObject.GetComponent<ICollectable>();
             collectable.Collect();
         }
-
-        //if (other.gameObject.CompareTag("Road Spawn Trigger"))
-        //{
-        //    Road road = RoadPool.Instance.Pool.Get();
-        //    road.transform.position = new Vector3(0, 0, 155f);
-        //}
     }
 }

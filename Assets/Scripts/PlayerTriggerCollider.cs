605b883f6e1960ac8420de427cc1f753cca803d6
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-09 22:57:57 +0300

    Trigger Functionality Setup

--- Assets/Scripts/PlayerTriggerCollider.cs
+++ Assets/Scripts/PlayerTriggerCollider.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerTriggerCollider : MonoBehaviour
 {
+    public static bool IS_INVULNERABLE { get; private set; } = false;
+
     Inventory _playerInventory;
 
 
@@ -9,15 +11,21 @@ public class PlayerTriggerCollider : MonoBehaviour
         _playerInventory = GetComponent<Inventory>();
     }
 
-    public static bool IS_INVULNERABLE { get; private set; } = false;
-
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Collectable")) 
         {
             ICollectable collectable = other.gameObject.GetComponent<ICollectable>();
-            collectable.Collect(_playerInventory);
+            collectable?.Collect(_playerInventory);
+            return;
+        }
+
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            ITrigger triggerable = other.gameObject.GetComponent<ITrigger>();
+            triggerable?.Trigger();
         }
     }
+
 
     public void SetInvulnerability(bool isInvulnerable) {
         IS_INVULNERABLE = isInvulnerable;

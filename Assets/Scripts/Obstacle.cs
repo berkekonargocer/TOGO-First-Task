b92185d8f7441aa374add27f5fb7ed3b97b4209d
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-10 01:51:52 +0300

    Obstacles set

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,14 +4,14 @@ using UnityEngine.Events;
 [DisallowMultipleComponent]
 public class Obstacle : MonoBehaviour, ITrigger
 {
-    [SerializeField] ScoreSystem playerScore;
+    [SerializeField] ScoreSystem playerScoreSystem;
     [SerializeField] UnityEvent onTriggered;
 
 
     public void Trigger() {
         if (!PlayerTriggerCollider.IS_INVULNERABLE)
         {
-            playerScore.DecrementScore(1);
+            playerScoreSystem.DecrementScore(1);
         }
 
         onTriggered?.Invoke();

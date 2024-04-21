954afd01376e26c146e35b84cc92daaa15e1e98b
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-21 12:37:17 +0300

    Jump Mechanic Implemented Completely

--- Assets/Scripts/GroundedCheck.cs
+++ Assets/Scripts/GroundedCheck.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GroundedCheck : MonoBehaviour
@@ -7,20 +6,36 @@ public class GroundedCheck : MonoBehaviour
     [SerializeField] Transform groundedCheckTransform;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Vector3 groundedCheckSize;
-    [SerializeField] float groundedCheckLength;
+
     public bool IsGrounded { get; private set; }
     RaycastHit[] _raycastHit = new RaycastHit[1];
 
+    public static event Action OnGrounded;
+    public static event Action OnLeftGround;
+
 
     void FixedUpdate() {
-        int hits = Physics.BoxCastNonAlloc(groundedCheckTransform.position, groundedCheckSize * 2, groundedCheckTransform.up * -1, _raycastHit, Quaternion.identity, groundedCheckLength, groundLayer);
+        int hits = Physics.BoxCastNonAlloc(groundedCheckTransform.position, groundedCheckSize * 2, groundedCheckTransform.up * -1, _raycastHit, Quaternion.identity, 0, groundLayer);
 
-        IsGrounded = hits > 0;
+        if (IsGrounded == false &&  hits == 1)
+        {
+            IsGrounded = true;
+            OnGrounded?.Invoke();
+            Debug.Log($"Is in ground: {IsGrounded}");
+            return;
+        }
+
+        if (IsGrounded == true && hits == 0)
+        {
+            IsGrounded = false;
+            OnLeftGround?.Invoke();
+        }
+
         Debug.Log($"Is in ground: {IsGrounded}");
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(groundedCheckTransform.position + groundedCheckTransform.up * -1 * groundedCheckLength, groundedCheckSize);
+        Gizmos.DrawWireCube(groundedCheckTransform.position + groundedCheckTransform.up * -1 * 0, groundedCheckSize);
     }
 }
--- Assets/Scripts/JumpOverObstacle.cs
+++ Assets/Scripts/JumpOverObstacle.cs
@@ -10,8 +10,11 @@ namespace NOJUMPO
         [SerializeField] LayerMask jumpObstacleLayer;
 
         [SerializeField] float jumpForce;
-        RaycastHit[] _raycastResult = new RaycastHit[1];
+
         Rigidbody _objectRigidbody;
+        GroundedCheck _groundedCheck;
+
+        RaycastHit[] _raycastResult = new RaycastHit[1];
 
         const float JUMP_FORCE_MULTIPLIER = 5.0f;
 
@@ -19,14 +22,27 @@ namespace NOJUMPO
         // ------------------------- UNITY BUILT-IN METHODS ------------------------
         void Awake() {
             _objectRigidbody = GetComponent<Rigidbody>();
+            _groundedCheck = GetComponent<GroundedCheck>();
+        }
+
+        void OnEnable() {
+            GroundedCheck.OnGrounded += SetGroundedDrag;
+            GroundedCheck.OnLeftGround += SetAirDrag;
+        }
+
+        private void OnDisable() {
+            GroundedCheck.OnGrounded -= SetGroundedDrag;
+            GroundedCheck.OnLeftGround -= SetAirDrag;
         }
 
         void FixedUpdate() {
             int hits = Physics.RaycastNonAlloc(raycastStartTransform.position, raycastStartTransform.forward, _raycastResult, obstacleCheckRayLength, jumpObstacleLayer);
 
-            for (int i = 0; i < hits; i++)
+            if (hits == 0)
+                return;
+
+            if (_groundedCheck.IsGrounded)
             {
-                Debug.Log("Hit " + _raycastResult[i].collider.gameObject.name);
                 Jump();
             }
         }
@@ -35,6 +51,14 @@ namespace NOJUMPO
         // ------------------------- CUSTOM PRIVATE METHODS ------------------------
         void Jump() {
             _objectRigidbody.AddForce(_objectRigidbody.transform.up * jumpForce * JUMP_FORCE_MULTIPLIER * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+
+        void SetGroundedDrag() {
+            Physics.gravity = new Vector3(Physics.gravity.x, -9.81f, Physics.gravity.z);
+        }
+
+        void SetAirDrag() {
+            Physics.gravity = new Vector3(Physics.gravity.x, -24.81f, Physics.gravity.z);
         }
     }
 }

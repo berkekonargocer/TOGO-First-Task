64a3bf6ba1d6454425328e18c482a636d035afa5
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-24 21:52:16 +0300

    Obstacle Triggering Twice Bug Fixed

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -7,6 +7,13 @@ public class Obstacle : MonoBehaviour, ITrigger
     [SerializeField] Inventory playerInventory;
     [SerializeField] UnityEvent onTriggered;
 
+    Collider _obstacleCollider;
+
+
+    void Awake() {
+        _obstacleCollider = GetComponent<Collider>();
+    }
+
 
     public void Trigger() {
         if (playerInventory.Items.Count == 0)
@@ -14,6 +21,7 @@ public class Obstacle : MonoBehaviour, ITrigger
             GameManager.Instance.LoseGame();
         }
 
+        _obstacleCollider.enabled = false;
         onTriggered?.Invoke();
         Destroy(gameObject);
     }

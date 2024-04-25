779100bd0ce03085438c1feef5570df25b9df2d2
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-25 12:51:04 +0300

    Inventory.cs AddItem Optimization

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -13,19 +13,21 @@ public class Inventory : MonoBehaviour
     public event Action<int> OnItemAmountChange;
 
     public void AddItem(ICollectable collectable) {
-        collectable.transform.SetParent(ItemCarryPosition);
+        Transform collectableTransform = collectable.transform;
+        collectableTransform.gameObject.GetComponent<Collider>().enabled = false;
+        collectableTransform.SetParent(ItemCarryPosition);
 
         if (Items.Count == 0)
         {
-            collectable.transform.localPosition = Vector3.zero;
+            collectableTransform.localPosition = Vector3.zero;
         }
         else
         {
             Transform lastItemTransform = Items.Peek().transform;
-            collectable.transform.localPosition = new Vector3(0, lastItemTransform.localPosition.y + lastItemTransform.localScale.y, 0);
+            collectableTransform.localPosition = new Vector3(0, lastItemTransform.localPosition.y + lastItemTransform.localScale.y, 0);
         }
 
-        collectable.transform.localRotation = Quaternion.Euler(new Vector3(collectable.transform.localRotation.x, Random.Range(0, 360), collectable.transform.localRotation.z));
+        collectableTransform.localRotation = Quaternion.Euler(new Vector3(collectableTransform.localRotation.x, Random.Range(0, 360), collectableTransform.localRotation.z));
 
         Items.Push(collectable);
         OnItemAmountChange?.Invoke(Items.Count);

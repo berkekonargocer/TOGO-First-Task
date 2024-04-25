08e095b26406c79e1b17ce7520759845182bfac3
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-25 16:15:18 +0300

    Finish Line Win Game Particle FX Implemented

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] ParticleSystem[] winGameParticleFX;
+
     private void OnTriggerExit(Collider other) {
         if (other.TryGetComponent(out Inventory inventory))
         {
             if(inventory.Items.Count > 0)
             {
+                for (int i = 0; i < winGameParticleFX.Length; i++)
+                {
+                    winGameParticleFX[i].Play();
+                }
+
                 GameManager.Instance.WinGame();
                 return;
             }

46e23e31fc1bf41ec08a97465a88215cbc899bb0
Berke Konargöçer <berke.konargocer@gmail.com>
2024-04-28 14:03:56 +0300

    SmoothFollow.cs Implemented

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -25,12 +25,34 @@ namespace NOJUMPO
         void Start() {
         }
 
-        void Update() {
+        void FixedUpdate() {
+            switch (followDirection)
+            {
+                case FollowDirection.X:
+                    transform.position = new Vector3(Mathf.Lerp(transform.position.x, transformToFollow.position.x, speed * Time.fixedDeltaTime), transform.position.y, transform.position.z) + offset;
+                    break;
+                case FollowDirection.Y:
+                    transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, transformToFollow.position.y, speed * Time.fixedDeltaTime), transform.position.z) + offset;
+                    break;
+                case FollowDirection.Z:
+                    transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(transform.position.z, transformToFollow.position.z, speed * Time.fixedDeltaTime)) + offset;
+                    break;
+                case FollowDirection.ALL:
+                    transform.position = Vector3.Lerp(transform.position, transformToFollow.position + offset, speed * Time.fixedDeltaTime);
+                    break;
+                default:
+                    break;
+            }
         }
 
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
-
+        public void Setup(Transform transformToFollow, Vector3 offset, FollowDirection direction, float speed) {
+            this.transformToFollow = transformToFollow;
+            this.offset = offset;
+            followDirection = direction;
+            this.speed = speed;
+        }
 
         // ------------------------ CUSTOM PROTECTED METHODS -----------------------
 
